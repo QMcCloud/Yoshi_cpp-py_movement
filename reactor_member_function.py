c79b89a78e6441395e5af8cfa0027b9058aaede2
harrisonmuller8 <126136905+harrisonmuller8@users.noreply.github.com>
2023-03-01 10:54:11 -0500

    Update reactor_member_function.py

--- reactor_member_function.py
+++ reactor_member_function.py
@@ -10,3 +10,5 @@ from geometry_msgs.msg import Vector3
 
 
 class Reactor(Node):
+
+#4 cases: one where both sides of the IR increase so it has to stop and turn, one where the left IR is increasing so it has to turn right, one where the right IR is increasing so it turns left, and one where both are clear so it can just move forward
