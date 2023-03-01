bcbaeaa10a87f3048bfc130c073aa79df6db41c1
QMcCloud <quinmccl@udel.edu>
2023-03-01 10:23:23 -0500

    Wander now works

--- wanderer_member_function.py
+++ wanderer_member_function.py
@@ -13,7 +13,10 @@
 # limitations under the License.
 
 from random import choice, uniform
+import sys
+
 import rclpy
+from rclpy import qos
 from rclpy.node import Node
 
 from std_msgs.msg import String
@@ -31,49 +34,66 @@ class Wanderer(Node):
             HazardDetectionVector,
             "/yoshi/hazard_detection",
             self.hazard_callback,
-            10)
+            qos.qos_profile_sensor_data
+            )
+        self.subscription  # prevent unused variable warning
         self.publisher_ = self.create_publisher(Twist, '/yoshi/cmd_vel', 10)
-        timer_period = 0.5  # seconds
+
+        timer_period = 0.25  # seconds
         self.timer = self.create_timer(timer_period, self.pub_cmd_vel_callback)
         
         self.twist = Twist()
         self.twist.angular = Vector3()
         self.twist.linear = Vector3()
 
-        self.twist.angular.x = 0
-        self.twist.angular.y = 0
-        self.twist.angular.z = 0
-        self.twist.linear.x = 0
-        self.twist.linear.y = 0
-        self.twist.linear.z = 0
+        self.twist.angular.x = 0.0
+        self.twist.angular.y = 0.0
+        self.twist.angular.z = 0.0
+        self.twist.linear.x = 0.1
+        self.twist.linear.y = 0.0
+        self.twist.linear.z = 0.0
 
-        self.subscription  # prevent unused variable warning
+        self.do_once_timer = None
+
+        self.prev_hazard = []
+
     
     def pub_cmd_vel_callback(self):
         self.publisher_.publish(self.twist)
         self.get_logger().info('Publishing: "%s"' % self.twist)
     
-    def do_once(self, func):
-        if(func) : func()
+    def do_once(self, func=None):
         self.do_once_timer.destroy()
+        self.get_logger().info('DOONCE FUNC!: "%s"' % func)
+        if(func) : func()
+        self.get_logger().info('IS DOONCE TIMER DEAD!: "%s"' % self.do_once_timer)
 
     def hazard_callback(self, msg : HazardDetectionVector):
-        self.get_logger().info('hazard!: "%i"' % len(msg.detections))
+        # if(len(msg.detections) > 0):
+        #     self.get_logger().info('hazardLen!: "%i"' % len(msg.detections))
         actions = {
-            HazardDetection.BUMP: self.handle_bump,
-            HazardDetection.BACKUP_LIMIT : self.handle_backup_limit,
-            HazardDetection.CLIFF: self.handle_bump,
-            HazardDetection.WHEEL_DROP : self.stop,
-            HazardDetection.STALL : self.handle_bump,
-            HazardDetection.OBJECT_PROXIMITY : self.handle_bump
+            HazardDetection.BUMP: ("BUMP",self.handle_bump),
+            HazardDetection.BACKUP_LIMIT : ("BACKUP_LIMIT",self.handle_backup_limit),
+            HazardDetection.CLIFF: ("CLIFF",self.handle_bump),
+            HazardDetection.WHEEL_DROP : ("WHEEL_DROP",self.stop),
+            HazardDetection.STALL : ("STALL",self.handle_bump),
+            HazardDetection.OBJECT_PROXIMITY : ("OBJECT_PROXIMITY",self.handle_bump)
         }
+        newHazardList = []
         for hazard in msg.detections:
-            action = actions.get(hazard, None)
-            if (action):
-                action()
-            self.get_logger().info('I heard: "%s"' % hazard)
+            newHazardList.append(hazard.type)
+            if (hazard.type not in self.prev_hazard):
+                hazardName, action = actions.get(hazard.type, None)
+                self.get_logger().info('hazardType!: "%s"' % hazardName)
+                self.get_logger().info('RUNNING ACTION!: "%s"' % action)
+                if (action):
+                    action()
+        self.prev_hazard = newHazardList
+        
+            
     
     def stop(self):
+        self.get_logger().info('STOP!')
         self.twist.angular.x = 0.0
         self.twist.angular.y = 0.0
         self.twist.angular.z = 0.0
@@ -83,6 +103,7 @@ class Wanderer(Node):
         self.publisher_.publish(self.twist)
     
     def reverse(self):
+        self.get_logger().info('REVERSE!')
         self.twist.angular.x = 0.0
         self.twist.angular.y = 0.0
         self.twist.angular.z = 0.0
@@ -92,6 +113,7 @@ class Wanderer(Node):
         self.publisher_.publish(self.twist)
     
     def forward(self):
+        self.get_logger().info('FORWARD!')
         self.twist.angular.x = 0.0
         self.twist.angular.y = 0.0
         self.twist.angular.z = 0.0
@@ -101,6 +123,7 @@ class Wanderer(Node):
         self.publisher_.publish(self.twist)
     
     def turn(self):
+        self.get_logger().info('TURN!')
         self.twist.angular.x = 0.0
         self.twist.angular.y = 0.0
         self.twist.angular.z = choice([-1.0, 1.0])
@@ -112,13 +135,15 @@ class Wanderer(Node):
     def handle_bump(self):
         self.stop()
         if self.do_once_timer : self.do_once_timer.destroy()
-        self.do_once_timer = self.create_timer(1, lambda : self.do_once(self.handle_backup_limit))
+        self.do_once_timer = self.create_timer(1.0, lambda : self.do_once(func = self.handle_backup_limit))
         self.reverse()
     
     def handle_backup_limit(self):
         self.stop()
         if self.do_once_timer : self.do_once_timer.destroy()
-        self.do_once_timer = self.create_timer(uniform(2.0943951, 4.1887902), lambda : self.do_once(self.forward))
+        delay = uniform(2.0943951, 4.1887902)
+        self.get_logger().info('WAIT! : %f' % delay)
+        self.do_once_timer = self.create_timer(delay, lambda : self.do_once(func = self.forward))
         self.turn()
 
 
